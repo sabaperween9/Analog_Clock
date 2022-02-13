87055e3b0ae350fb6b5ccf86d38fceaa68aabd77
saba perween <81516143+sabaperween9@users.noreply.github.com>
2022-02-13 21:29:20 +0530

    Add files via upload

---
+++ AnalogClock/Analog.js
@@ -0,0 +1,21 @@
+setInterval(()=>{
+    d=new Date;
+    htime=d.getHours();
+    mtime=d.getMinutes();
+    stime=d.getSeconds();
+
+    hrotation=30*htime+mtime/2;
+
+    mrotation=6*mtime;
+
+    srotation=6*stime;
+
+    const hour=document.getElementById("hour");
+    const minute=document.getElementById("minute");
+    const second=document.getElementById("second");
+
+    hour.style.transform=`rotate(${hrotation}deg)`;
+    minute.style.transform=`rotate(${mrotation}deg)`;
+    second.style.transform=`rotate(${srotation}deg)`;
+
+},1000)
